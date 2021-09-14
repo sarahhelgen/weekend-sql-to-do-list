d4d31939050f0862da2620e6801007375729d0b5
Sarah Helgen <sarah.helgen@gmail.com>
2021-09-13 19:10:00 -0500

    configuring heroku deployment

--- server/public/routes/tasks.router.js
+++ server/public/routes/tasks.router.js
@@ -1,22 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const pg = require('pg');
-const pool = new pg.Pool({
-    database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: '5432',
-    max: 10,
-    idleTimeoutMillis: 30000,
-});
-
-pool.on('connect', () => {
-    console.log('PG connected to postgres!');
-});
-
-pool.on('error', (error) => {
-    console.log('Unable to connect to postgres!', error);
-});
 
 router.post('/', (req, res) => {
     console.log('in /tasks POST');
